f3fd0ce4c605bb2d0e71cf1ee87faaf368c12e4a
mahbubhswe <mahbubhswe@gmail.com>
2023-05-10 09:08:00 +0600

    up

--- pages/api/patient/ambulance/readAmbulanceForBooking.js
+++ pages/api/patient/ambulance/readAmbulanceForBooking.js
@@ -1,5 +1,5 @@
 import nc from "next-connect";
-import { prisma } from "../../../../utils/db.js";
+import { prisma } from "../../../../utils/db.ts";
 const handler = nc();
 handler.get(async (req, res) => {
   try {
